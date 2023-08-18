7cea254c28ccc65afdb4982f0af925c6ef3a8827
Md.Raju Biswas <91734699+raju-2514@users.noreply.github.com>
2023-08-18 20:48:15 +0600

    Add files via upload

---
+++ 2nd.py
@@ -0,0 +1,7 @@
+def main():
+    print(125)
+    name=input("what is your name? :")
+    print("Nice to meet you",name)
+if __name__=="__main__":
+    main()
+  
---
+++ variable.py
@@ -0,0 +1,23 @@
+myint=12
+myfloat=12.32
+mystring="my name"
+myseries=[1,3.2,"raju",748]
+myseries2=("name",12.54)
+mydic={"one":1,"two":2}
+
+#printing the value of variable
+# print(myint)
+# print(myfloat)
+# print(mystring)
+# print(myseries)
+# print(myseries2)
+# print(mydic)
+
+#concatanatoion in same vaiable type is possible 
+# print("string name"+str(124))
+
+#Global vs local variable
+def myfunction() :
+         mystring="def"
+         print(mystring)
+
