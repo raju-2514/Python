d0a778d4e5bc6f09cdf349e464c0f2bf8c81d303
Md.Raju Biswas <91734699+raju-2514@users.noreply.github.com>
2023-08-20 10:33:05 +0600

    Add files via upload

---
+++ condition.py
@@ -0,0 +1,13 @@
+#if condition
+def mis():
+   x,y=15,15
+   if x < y:
+         result ="x is less than Y"
+   elif x==y :
+          result="those two value are same"
+   else:
+         result="X is not less than Y"      
+    
+   print(result)
+
+mis()
